259d3b432e1424f282ef4dd97dfc64023458e513
patricia simons <trishasimons16@gmail.com>
2020-05-04 17:36:11 -0400

    save new changes

---
+++ assets/javascript/game.js
@@ -0,0 +1,45 @@
+let restButton = document.querySelector('numResets');
+restButton.addEventListener('click', function(){
+    console.log('+button clicked');
+})
+let resetContainer = document.querySelector('reset');
+resetContainer.addEventListener('click', function(){
+    console.log('+button clicked');
+})
+
+
+
+
+
+
+
+
+
+
+teamoneButton.addEventListener("click", function() {
+    count = parseInt(teamoneNumShots.innerText) + 1;
+    teamoneNumShots.innerText = count;
+
+
+
+
+
+let shotsTaken = document.querySelector('#teamone-numshots');
+let goals = document.querySelector('#teamone-numgoals');
+
+let teamoneShootButton = document.querySelector('#goals');
+
+shotsTaken.addEventListener('click',function(){
+console.log('+button clicked');
+
+
+
+})
+
+shotsTaken.addEventListener("click", function(){
+    console.log("+button clicked");
+    let shootsTaken= Number(counter.innerHTML) + 1;
+})
+
+
+
